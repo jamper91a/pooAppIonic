5c1096d7c1a5e395aca83159a1cbe306e124476c
Jorge Andres Moreno Jaimes <jamper91a@gmail.com>
2020-12-17 09:11:35 +1300

    Implementing angular material an log in page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { MaterialModule } from '../material.module';
 
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule
-  ],
-  declarations: [HomePage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        HomePageRoutingModule,
+        MaterialModule,
+        ReactiveFormsModule,
+    ],
+  declarations: [HomePage],
 })
 export class HomePageModule {}
