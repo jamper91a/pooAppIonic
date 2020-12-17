0901599b0d7060a08003b29b9082387978d56560
Jorge Andres Moreno Jaimes <jamper91a@gmail.com>
2020-12-17 17:01:59 +1300

    Implementing map to check the location of the last download

--- src/app/pages/my-tanks/my-tanks.page.ts
+++ src/app/pages/my-tanks/my-tanks.page.ts
@@ -1,21 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {TanksByClientResponse} from '../../api/responses/TanksByClientResponse';
 import {ClientService} from '../../api/service/client.service';
 import {Router} from '@angular/router';
 import {Util} from '../../providers/util';
-
+import { Map, tileLayer, marker, icon } from 'leaflet';
+import {Platform} from '@ionic/angular';
 @Component({
   selector: 'app-my-tanks',
   templateUrl: './my-tanks.page.html',
   styleUrls: ['./my-tanks.page.scss'],
 })
-export class MyTanksPage implements OnInit {
+export class MyTanksPage implements OnInit, AfterViewInit {
 
   response: TanksByClientResponse = new TanksByClientResponse();
   constructor(
       private clientService: ClientService,
       private router: Router,
-      private util: Util
+      private util: Util,
+      public platform: Platform,
   ) { }
 
   async ngOnInit() {
@@ -24,12 +26,44 @@ export class MyTanksPage implements OnInit {
 
   async getMyTanks() {
     try {
+
       this.response  = await this.clientService.getTanks();
-      console.log(this.response);
+      this.initMap();
     } catch (e) {
-      this.util.showToast('Error getting the tanks');
-      this.router.navigateByUrl('home');
+      await this.util.showToast('Error getting the tanks');
+      await this.router.navigateByUrl('home');
     }
+  }
+
+  ngAfterViewInit(): void {
+    this.platform.ready().then(() => {
+    });
+  }
+
+  initMap() {
+    const self = this;
+    setTimeout(() => {
+      for (const tank of self.response.tanks){
+        // const map = new Map('map' + tank.id).setView([tank.history.position.x, tank.history.position.y], 5);
+        const map = new Map('map' + tank.id, {
+          center: [tank.history.position.x, tank.history.position.y],
+          zoom: 5,
+          zoomControl: false,
+          doubleClickZoom: false,
+          dragging: false
+        });
+        tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+        const customMarkerIcon = icon({
+          iconUrl: 'assets/images/pin.png',
+          iconSize: [24, 24],
+          popupAnchor: [0, -20]
+        });
+        marker([tank.history.position.x, tank.history.position.y], {icon: customMarkerIcon})
+            .addTo(map);
+      }
+    });
   }
 
 }
